d78124a1e66fb7ef55b4af73014fa5bb3bcaecbd
pquigle <pquigle@uwo.com>
2022-10-21 12:49:31 -0400

    Changed colibri_primary_filter to colibri_photometry

--- setup_colibri_primary_filter.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(ext_modules = cythonize('colibri_primary_filter.pyx',annotate=True),include_dirs=[numpy.get_include()])
