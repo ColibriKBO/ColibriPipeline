ab762f4943139af1f99098ed35a41a9e3746b983
mjmazur <mmazur5@uwo.ca>
2022-11-28 17:34:20 +0000

    Update VizieR_query.py
    increased Gmag limit

--- VizieR_query.py
+++ VizieR_query.py
@@ -23,7 +23,7 @@ def makeQuery(field_centre, SR):
     result = Vizier.query_region(coord.SkyCoord(ra=field_centre[0], dec=field_centre[1], 
                                             unit=(u.deg, u.deg), frame = 'icrs'),  
                              radius=coord.Angle(SR, "deg"), catalog='I/345/gaia2',
-                             column_filters={'Gmag': '<13'})
+                             column_filters={'Gmag': '<16'})
 
     table = result['I/345/gaia2']
     query = np.array(table['RA_ICRS','DE_ICRS','Gmag', 'BP-RP'])
