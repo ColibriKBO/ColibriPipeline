89750ebc38f3e30fd7eda66d26db021ece38ec15
mjmazur <mmazur5@uwo.ca>
2023-01-27 16:12:52 +0000

    Revert "Update colibri_main_py3.py"
    This reverts commit 87996d7fa0884ea566063cbdf381975b629f87ab.

--- colibri_main_py3.py
+++ colibri_main_py3.py
@@ -313,14 +313,11 @@ def firstOccSearch(minuteDir, MasterBiasList, kernel, exposure_time, sigma_thres
     drift_times.append(first_drift[1])
 
     #refined star positions and times for last image
-    drift_multiple = 1  # relaxation factor on our centroid finding
+    drift_multiple = 10  # relaxation factor on our centroid finding
     last_drift = cp.refineCentroid(lframe_data,lframe_time[0], drift_pos[0], GaussSigma*drift_multiple)
     drift_pos[1] = last_drift[0]
     drift_times.append(last_drift[1])
     drift_times = Time(drift_times, precision=9).unix
-
-    # print(len(drift_pos[0]))
-    # print(len(drift_pos[1]))
 
     #get median drift rate [px/s] in x and y over the minute
     x_drift, y_drift = cp.averageDrift(drift_pos[0],drift_pos[1], drift_times[0],drift_times[1])
@@ -389,28 +386,24 @@ def firstOccSearch(minuteDir, MasterBiasList, kernel, exposure_time, sigma_thres
                                                     (x_length, y_length),
                                                     (x_drift, y_drift))        
         
-            
+        print("DRIFTED DATA SAVED")    
         gc.collect()
         
         import matplotlib.pyplot as plt
         for i in range(len(starData[0])):
             flux=[]
             frame=[]
-            x_coords_starfind=starData[0][i][0]
-            y_coords_starfind=starData[0][i][1]
-            #x_coords_fi=starData[-1][i][0]
-            #y_coords_fi=starData[-1][i][1]
-            x_coords_in=first_drift[0][i][0]
-            y_coords_in=first_drift[0][i][1]
-            x_coords_fi=last_drift[0][i][0]
-            y_coords_fi=last_drift[0][i][1]
+            x_coords_in=starData[0][i][0]
+            y_coords_in=starData[0][i][1]
+            x_coords_fi=starData[-1][i][0]
+            y_coords_fi=starData[-1][i][1]
             for j in range(len(starData)):
                 flux.append(starData[j][i][2])
                 frame.append(j)
                 
         
             fig, ax1 = plt.subplots()
-            ax1.scatter(frame, flux,label="starfind pos: x="+str(x_coords_starfind)+" y="+str(y_coords_starfind)+"\n initial pos: x="+str(x_coords_in)+" y="+str(y_coords_in)+"\n final pos: x="+str(x_coords_fi)+" y="+str(y_coords_fi))
+            ax1.scatter(frame, flux,label="initial pos: x="+str(x_coords_in)+" y="+str(y_coords_in)+"\n final pos: x="+str(x_coords_fi)+" y="+str(y_coords_fi))
             plt.legend()
             plot_path=base_path.joinpath('ColibriArchive', str(obs_date), minuteDir.name)
             if not os.path.exists(plot_path):
@@ -454,7 +447,7 @@ def firstOccSearch(minuteDir, MasterBiasList, kernel, exposure_time, sigma_thres
                 
         gc.collect()
     
-        import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
     # for starNum in range(0,num_stars):
     #     flux=starData[:, starNum, 2]
     #     x_coords_in=starData[0][starNum][0]
@@ -474,21 +467,17 @@ def firstOccSearch(minuteDir, MasterBiasList, kernel, exposure_time, sigma_thres
         for i in range(len(starData[0])):
             flux=[]
             frame=[]
-            x_coords_starfind=starData[0][i][0]
-            y_coords_starfind=starData[0][i][1]
-            #x_coords_fi=starData[-1][i][0]
-            #y_coords_fi=starData[-1][i][1]
-            x_coords_in=first_drift[0][i][0]
-            y_coords_in=first_drift[0][i][1]
-            x_coords_fi=last_drift[0][i][0]
-            y_coords_fi=last_drift[0][i][1]
+            x_coords_in=starData[0][i][0]
+            y_coords_in=starData[0][i][1]
+            x_coords_fi=starData[-1][i][0]
+            y_coords_fi=starData[-1][i][1]
             for j in range(len(starData)):
                 flux.append(starData[j][i][2])
                 frame.append(j)
                 
         
             fig, ax1 = plt.subplots()
-            ax1.scatter(frame, flux,label="starfind pos: x="+str(x_coords_starfind)+" y="+str(y_coords_starfind)+"\n initial pos: x="+str(x_coords_in)+" y="+str(y_coords_in)+"\n final pos: x="+str(x_coords_fi)+" y="+str(y_coords_fi))
+            ax1.scatter(frame, flux,label="initial pos: x="+str(x_coords_in)+" y="+str(y_coords_in)+"\n final pos: x="+str(x_coords_fi)+" y="+str(y_coords_fi))
             plt.legend()
             plot_path=base_path.joinpath('ColibriArchive', str(obs_date), minuteDir.name)
             if not os.path.exists(plot_path):
