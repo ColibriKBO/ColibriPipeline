0eca2ee823b67e79cb4ff30ac821b1c28a19d316
mjmazur <mmazur5@uwo.ca>
2023-06-12 16:59:21 +0000

    Extensive bugfixing and type correcting

--- generate_specific_lightcurve.py
+++ generate_specific_lightcurve.py
@@ -79,18 +79,20 @@ def generateLightcurve(minute_dir, central_frame, master_bias_list,
     dark = cir.chooseBias(minute_dir, master_bias_list, datetime.strptime(obsdate,OBSDATE_FORMAT))
 
     # Get image dimensions & number of images
-    x_length, y_length, num_images = cir.getSizeRCD(str(minute_dir))
+    image_paths = sorted(minute_dir.glob('*.rcd'))
+    x_length, y_length, num_images = cir.getSizeRCD(image_paths)
 
     # Determine frames to save
-    min_frame = int(central_frame - (SEC_TO_SAVE // EXPOSURE_TIME))
+    min_frame = int(central_frame - 1 - (SEC_TO_SAVE // EXPOSURE_TIME))
     max_frame = int(central_frame + 1 + (SEC_TO_SAVE // EXPOSURE_TIME))
-    if min_frame < EXCLUDE_IMAGES:
-        min_frame = EXCLUDE_IMAGES
+    if min_frame <= EXCLUDE_IMAGES:
+        min_frame = EXCLUDE_IMAGES + 1
     if max_frame >= num_images:
         max_frame = None
+    print(f"Saving frames from {min_frame} to {max_frame if max_frame is not None else num_images}")
+    print(f"    with central frame {central_frame}.")
 
     # Get image paths to process
-    image_paths = sorted(minute_dir.glob('*.rcd'))
     lightcurve_paths = image_paths[min_frame:max_frame]
     num_frames = len(lightcurve_paths)
 
@@ -122,47 +124,58 @@ def generateLightcurve(minute_dir, central_frame, master_bias_list,
 
     # Refine star positions for first and final frames
     # TODO: Fix the arguments here!!!
-    first_positions, _ = cp.refineCentroid(first_frame, first_time[0], initial_positions[:,0], mean_guass_sigma)
-    final_positions, _ = cp.refineCentroid(final_frame, final_time[0], initial_positions[:,1], mean_guass_sigma)
+    first_positions, _ = cp.refineCentroid(first_frame, first_time[0], initial_positions, mean_guass_sigma)
+    final_positions, _ = cp.refineCentroid(final_frame, final_time[0], initial_positions, mean_guass_sigma)
 
     # Calculate median drift rate [px/s] in x and y over the minute
-    x_drift, y_drift = cp.averageDrift(first_positions, final_positions, first_time, final_time)
+    x_drift, y_drift = cp.averageDrift(first_positions, final_positions, 
+                                       Time(first_time, precision=9).unix, 
+                                       Time(final_time, precision=9).unix)
 
     # Check if drift is within tolerance and if a correction is needed
     if (abs(x_drift) > DRIFT_TOLERANCE) or (abs(y_drift) > DRIFT_TOLERANCE):
-        print(f"WARNING: Drift rate is too high for {minute_dir.name}")
+        print(f"ERROR: Drift rate is too high for {minute_dir.name}!")
         return None
     elif (abs(x_drift) > DRIFT_THRESHOLD) or (abs(y_drift) > DRIFT_THRESHOLD):
+        print(f"WARNING: Drift detected in {minute_dir.name}!")
+        print(f"         Drifted by ({x_drift},{y_drift})")
         drift = True
     else:
+        print("No drift detected :)")
         drift = False
 
     
     ## Generate WCS transformations ##
 
     star_X,star_Y = xy
+    print(f"(StarX, StarY) = ({star_X},{star_Y})")
 
     # Determine if star is visible in field of view
     if (star_X < 0) or (star_X > x_length) or (star_Y < 0) or (star_Y > y_length):
         print(f"WARNING: Star is not visible in {minute_dir.name}")
         return None
-    elif np.all(abs(first_positions[0] - star_X) > APERTURE_RADIUS) or \
-         np.all(abs(first_positions[1] - star_Y) > APERTURE_RADIUS):
+    elif np.all(abs(first_positions[:,0] - star_X) > APERTURE_RADIUS) or \
+         np.all(abs(first_positions[:,1] - star_Y) > APERTURE_RADIUS):
         print(f"WARNING: Star was not found by SEP in {minute_dir.name}")
         return None
+    else:
+        target_star_index_x = np.where(abs(first_positions[:,0] - star_X) < APERTURE_RADIUS)
+        target_star_index_y = np.where(abs(first_positions[:,1] - star_Y) < APERTURE_RADIUS)
+        target_star_radius  = initial_radii[np.intersect1d(target_star_index_x,target_star_index_y)]
 
     ## Generate lightcurve ##
 
     # Initialize lightcurve container
     # [0] x coorinate; [1] y coordinate; [2] flux; [3] frame time
-    star_data = np.empty((num_images, 4), dtype=np.float64)
+    star_data = np.empty((num_frames+1, 1, 4), dtype=np.float64)
+    print(f"num_images = {num_frames}")
 
     # Seed lightcurve with first frame data
-    star_initial_flux = sep.sum_circle(first_frame, star_X, star_Y, initial_radii)[0]
-    star_data[0] = [star_X, star_Y, star_initial_flux, first_time[0].unix]
+    star_initial_flux = sep.sum_circle(first_frame, [star_X], [star_Y], [target_star_radius])
+    star_data[0] = [[star_X, star_Y, star_initial_flux[0][0,0], Time(first_time, precision=9).unix]]
 
     # Loop through each frame and calculate flux
-    header_times = [first_time]
+    header_times = [first_time[0]]
     bkg_medians  = []
     if drift: # If drift correction is needed
 
@@ -171,15 +184,16 @@ def generateLightcurve(minute_dir, central_frame, master_bias_list,
             image_data,image_time = cir.importFramesRCD(lightcurve_paths,i,1,dark)
 
             # Analyze data
+            #print("STARDATA: ",star_data[i])
             star_data[i+1] = cp.timeEvolve(image_data,
                                            deepcopy(star_data[i]),
                                            image_time[0],
                                            APERTURE_RADIUS,
-                                           num_stars,
+                                           1,
                                            (x_length,y_length),
                                            (x_drift,y_drift))
             header_times = header_times + image_time
-            bkg_medians = bkg_medians.append(np.median(image_data))
+            bkg_medians.append(np.median(image_data))
 
     else: # if drift correction is not needed
 
@@ -188,29 +202,33 @@ def generateLightcurve(minute_dir, central_frame, master_bias_list,
             image_data,image_time = cir.importFramesRCD(lightcurve_paths,i,1,dark)
 
             # Analyze data
+            #print("STARDATA: ",star_data[i])
             star_data[i+1] = cp.getStationaryFlux(image_data,
                                                   deepcopy(star_data[i]),
                                                   image_time[0],
                                                   APERTURE_RADIUS,
-                                                  num_stars,
+                                                  1,
                                                   (x_length,y_length),
                                                   (x_drift,y_drift))
             header_times = header_times + image_time
-            bkg_medians = bkg_medians.append(np.median(image_data))
+            bkg_medians.append(np.median(image_data))
 
 
     # Return relevant data, optionally background median
+    # Removes the zeroeth frame data used to seed the star_data
     #return lightcurve_paths, star_data, header_times, np.median(bkg_medians)
-    return lightcurve_paths, star_data, header_times
+    return lightcurve_paths, star_data[1:], header_times[1:]
 
 
 def saveLightcurve(lightcurve_paths, star_data, header_times,
                    obsdate, radec, xy):
 
+    print(f"\n# Write lightcurve to file #")
+
     ## Analyze lightcurve ##
 
     # Get statistical parameters of lightcurve
-    lightcurve_flux = star_data[:,2]
+    lightcurve_flux = star_data[:,0,2]
     lightcurve_mean = np.mean(lightcurve_flux)
     lightcurve_std  = np.std(lightcurve_flux)
 
@@ -226,14 +244,16 @@ def saveLightcurve(lightcurve_paths, star_data, header_times,
     # Naming/indexing parameters
     center_index = len(lightcurve_paths)//2
     obsdate_hyphon = hyphonateDate(obsdate)
-    timestamp_bare = header_times[center_index].strftime(BARE_FORMAT)
-    field_name = lightcurve_paths[center_index].split("_")[0]
+
+    center_timestamp = datetime.strptime(header_times[center_index][:-3], TIMESTAMP_FORMAT)
+    timestamp_bare   = center_timestamp.strftime(BARE_FORMAT)
+    
+    field_name = (lightcurve_paths[center_index].name).split("_")[0]
 
     # Save names
     save_path = ARCHIVE_PATH / obsdate_hyphon
-    save_file = save_path / "det_{}_{}_FORCED_{}.txt".format(obsdate_hyphon,
-                                                             timestamp_bare,
-                                                             telescope)
+    save_file = save_path / "det_{}_FORCED_{}.txt".format(timestamp_bare, telescope)
+    print(f"Saving lightcurve to {save_file}")
 
     # Write header and data to detection file
     with open(save_file, 'w') as filehandle:
@@ -246,7 +266,7 @@ def saveLightcurve(lightcurve_paths, star_data, header_times,
         filehandle.write('#    DATE-OBS: %s\n' %(header_times[center_index]))
         filehandle.write('#    Telescope: %s\n' %(telescope))
         filehandle.write('#    Field: %s\n' %(field_name))
-        filehandle.write('#    significance: ARTICICAL\n')
+        filehandle.write('#    significance: ARTIFICAL\n')
         filehandle.write('#    Raw lightcurve std: %.4f\n' %(lightcurve_std))
         filehandle.write('#    Raw lightcurve mean: %.4f\n' %(lightcurve_mean))
         filehandle.write('#    Convolution background std: NONE\n')
@@ -257,12 +277,12 @@ def saveLightcurve(lightcurve_paths, star_data, header_times,
 
         # Write data
         print("Writing lightcurve data...")
-        for i in len(lightcurve_paths):
-            seconds = star_data[i,3].strftime('%S')
-            filehandle.write(f"{lightcurve_paths[i]} {seconds} {star_data[i,2]} N/A")
+        for i in range(len(lightcurve_paths)):
+            seconds = header_times[i].split(":")[-1]
+            filehandle.write(f"{lightcurve_paths[i]} {seconds} {star_data[i,0,2]} N/A\n")
             
     # Completed
-    print(f"Write curve written to {save_file}")
+    print(f"Finished writing.")
 
 
 def findMinute(obsdate, timestamp):
@@ -372,7 +392,6 @@ if __name__ == '__main__':
 
     # Assign found_minute to variables
     minute_dir,peak_frame = found_minute
-    print(peak_frame, type(peak_frame))
 
     # Get XY pixel coordinates for star of interest from WCS transform
     star_XY = reversePixelMapping(minute_dir, obsdate, radec[0], radec[1])
@@ -380,7 +399,11 @@ if __name__ == '__main__':
     # Generate lightcurve
     lightcurve = generateLightcurve(minute_dir,peak_frame,mbias_array,
                                     obsdate,star_XY)
-    lightcurve_paths,star_data,header_times = lightcurve
+    if lightcurve is None:
+        print("ERROR: Failed to generate lightcurve.")
+        sys.exit()
+    else:
+        lightcurve_paths,star_data,header_times = lightcurve
 
     # Save lightcurve
     saveLightcurve(lightcurve_paths, star_data, header_times,
