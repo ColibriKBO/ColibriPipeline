a1ada115490ce491c584f0c261767d16ffcf227f
pquigle <pquigle@uwo.com>
2023-06-21 13:14:24 -0400

    Moved transform functions from getRAdec.py to coordsfinder.py

--- wcsmatching.py
+++ wcsmatching.py
@@ -6,6 +6,7 @@ Created:    June 19, 2022
 Updated:    June 20, 2022
     
 Usage: python wcsmatching.py <obs_date>
+       *This script is intended to run only on BLUEBIRD
 """
 
 # Module Imports
@@ -33,8 +34,6 @@ from coordsfinder import getTransform
 # Disable Warnings
 import warnings
 import logging
-#warnings.filterwarnings("ignore",category=DeprecationWarning)
-#warnings.filterwarnings("ignore",category=VisibleDeprecationWarning)
 
 #-------------------------------global vars-----------------------------------#
 
@@ -43,3 +42,28 @@ BASE_PATH = pathlib.Path('D:/')
 DATA_PATH = BASE_PATH / 'ColibriData'
 IMGE_PATH = BASE_PATH / 'ColibriImages'
 ARCHIVE_PATH = BASE_PATH / 'ColibriArchive'
+
+
+#--------------------------------functions------------------------------------#
+
+def npyFileWCSUpdate(npy_file_list):
+
+    # Iterate through npy files and generate a WCS transformation to write
+    # back into the WCS file
+    for npy_file in npy_file_list:
+        # Get minute string from filename
+        timestamp = re.search("^(\d{6}\._\d{2}\.\d{2}\.\d{2}\.\d{3})", npy_file.name)
+
+        # Read in data from npy file
+        
+
+        # Generate WCS transformation and the RA/Dec
+        transform  = getTransform(timestamp, npy_file, {})
+        star_radec = getRAdec.getRAdec(transform, star_find_results)
+
+        ## Save the array of star positions as an .npy file
+        ## Format: x  |  y  | half light radius | ra | dec
+        star_pos_file = BASE_PATH.joinpath('ColibriArchive', str(obs_date), minuteDir.name + '_' + str(detect_thresh) + 'sig_pos.npy')
+        if star_pos_file.exists():
+            star_pos_file.unlink()
+        np.save(star_pos_file, star_radec)
