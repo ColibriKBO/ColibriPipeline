5ac37ff9bd7908191402a04ec44e4d6efe6bb20d
pquigle <pquigle@uwo.com>
2023-12-07 14:35:53 -0500

    Propagated npy changes to all other functions

--- read_npy.py
+++ read_npy.py
@@ -11,33 +11,47 @@ for looking at Colibri pipeline output files
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from glob import glob
 
 
 def read_plot(filename):
     '''reads .npy file and makes plot of star positions
+    Should update the polar projection plot according to :
+        https://stackoverflow.com/questions/29525356/produce-a-ra-vs-dec-equatorial-coordinates-plot-with-python
+
     input: path to .npy file (path object)
     returns: shows (or saves) plot of star positions found in file'''
     
 
-    star_pos1 = np.load(filename)
+    # Extract star data
+    star_pos = np.load(filename)
 
-    print(star_pos1[:,0])
-    
-    plt.scatter(star_pos1[:,0], star_pos1[:,1])
+    # Plot detected stars from image
+    plt.scatter(star_pos[:,0], star_pos[:,1])
 
+    plt.title('Star Pixel Positions')
     plt.xlabel('X')
     plt.ylabel('Y')
-
+    plt.xlim(0, 2048)
+    plt.ylim(0, 2048)
+    plt.grid()
     plt.show()
-    plt.close()
 
-    #plt.scatter(range(0, len(star_pos1[:,0])), star_pos1[:,2])
-    #plt.xlabel('Star number')
-    #plt.ylabel('Half light radius (px)')
+    # Plot ra and dec of stars on circular plot
+    ra = star_pos[:,4]*np.pi/180.
+    dec = star_pos[:,5]
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='polar')
+    ax.scatter(ra, dec)
+    ax.set_rmax(90)
+    ax.set_rticks([0, 30, 60, 90])
+    ax.set_rlabel_position(0)
+    ax.grid(True)
+    ax.set_title('Star RA and Dec')
     plt.show()
-    plt.close()
+
 
 
 def to_ds9(filename, savefile):
